1ce26691b61fd58289fa03c44a2539d84c4b3cc4
Nanorii <nadjariis@hotmail.com>
2018-11-15 13:33:34 +0100

    Door

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -7,16 +7,35 @@ public class KeyController : MonoBehaviour {
 	public GameObject door;
 	private SpriteRenderer spriteRenderer;
 	private PolygonCollider2D col;
+	public float doorSpeed = 0f;
+    public Vector2 doorInitialDirection;
+    Rigidbody2D rig2D;
+	private Vector2 doorPos;
+	private float doorHeight;
+	private bool open;
+	
 
 	// Use this for initialization
 	void Start () {
 		//levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
 		this.spriteRenderer = GetComponent<SpriteRenderer>();
 		this.col = GetComponent<PolygonCollider2D>();
+		
+		rig2D = door.GetComponent<Rigidbody2D>();
+        rig2D.velocity = doorInitialDirection * new Vector2(doorSpeed, doorSpeed);
+		doorPos = door.transform.position;
+		doorHeight = door.GetComponent<SpriteRenderer>().bounds.size.y;
+		open = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if((door.transform.position.y > doorPos.y + doorHeight) && !open) {
+			doorSpeed = 0f;
+			rig2D.velocity = doorInitialDirection * new Vector2(doorSpeed, doorSpeed);
+			open = true;
+		}
 		
 	}
 
@@ -25,8 +44,10 @@ public class KeyController : MonoBehaviour {
         if(other.gameObject.tag == "Player") {
 			spriteRenderer.enabled = false;
 			col.enabled = false;
-			door.SetActive(false);
+
+			doorSpeed = 2f;
+			rig2D.velocity = doorInitialDirection * new Vector2(doorSpeed, doorSpeed);
         }
     }
-	
+
 }
