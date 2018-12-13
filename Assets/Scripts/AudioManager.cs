9b0200a10c0c30e917607c9569da6b48b6af0d75
Zaggralez <jacob@pjetursson.com>
2018-12-13 22:20:57 +0100

    night and day changes. music stuff

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,9 +6,8 @@ public class AudioManager : MonoBehaviour {
     private static AudioManager instance;
     private static AudioSource caveMusic;
     private static AudioSource houseMusic;
-    private static AudioSource westernMusic;
     private static AudioSource currMusic;
-    public static string[] musicStr = { "western", "western", "cave", "cave", "cave", "house", "house", "house" };
+    public static string[] musicStr = { "house", "house", "house", "cave", "cave", "cave", "house", "house" };
 
     void Awake()
     {
@@ -20,8 +19,7 @@ public class AudioManager : MonoBehaviour {
         }
         houseMusic = GetComponents<AudioSource>()[0];
         caveMusic = GetComponents<AudioSource>()[1];
-        westernMusic = GetComponents<AudioSource>()[2];
-        currMusic = westernMusic;
+        currMusic = houseMusic;
         DontDestroyOnLoad(this.gameObject);
     }
 
@@ -37,19 +35,13 @@ public class AudioManager : MonoBehaviour {
             houseMusic.Play();
             currMusic = houseMusic;
         }
-        else if (music == "western" && currMusic != westernMusic)
-        {
-            currMusic.Stop();
-            westernMusic.Play();
-            currMusic = westernMusic;
-        }
     }
 
     public static void playMenuMusic() {
-        if (currMusic != westernMusic) {
+        if (currMusic != houseMusic) {
             currMusic.Stop();
-            westernMusic.Play();
-            currMusic = westernMusic;
+            houseMusic.Play();
+            currMusic = houseMusic;
         }
     }
 }
